8bfb7bdf5ca3f793b6de0495a2b583a873af48c1
Vishesh <vishesht27@gmail.com>
2022-06-02 13:05:20 +0530

    Added cards and material you

--- app/src/main/java/com/example/material3_demo/MainActivity.kt
+++ app/src/main/java/com/example/material3_demo/MainActivity.kt
@@ -11,22 +11,53 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.material3_demo.ui.ImageCard
 import com.example.material3_demo.ui.theme.Material3AppTheme
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Material3AppTheme {
-                // A surface container using the 'background' color from the theme
-                 Surface(
+                Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Scaffold(
+                        floatingActionButton = {
+                            FloatingActionButton(onClick = {}) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.onPrimaryContainer
+                                )
+                            }
+                        },
+                        topBar = {
+                            SmallTopAppBar(
+                                title = {
+                                    Text(text = "Material 3")
+                                },
+                                colors = TopAppBarDefaults.smallTopAppBarColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                    titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            )
+                        }
+                    ) { values ->
+                        LazyColumn(contentPadding = values) {
+                            items(20) {
+                                ImageCard(
+                                    title = "Bacon ipsum",
+                                    description = "Bacon ipsum dolor amet pork shankle beef andouille ball tip. Meatball corned beef swine, strip steak bacon jerky doner tongue biltong pork loin drumstick sausage hamburger burgdoggen.",
+                                    modifier = Modifier.padding(16.dp)
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
-
--- app/src/main/java/com/example/material3_demo/ui/ImageCard.kt
+++ app/src/main/java/com/example/material3_demo/ui/ImageCard.kt
@@ -1,11 +1,25 @@
 package com.example.material3_demo.ui
 
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.flowlayout.SizeMode
+import kotlin.random.Random
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ImageCard(
     title:String,
@@ -20,6 +34,65 @@ fun ImageCard(
         ),
         shape = MaterialTheme.shapes.large
     ) {
-        
+        Image(
+            painter = rememberAsyncImagePainter(
+                model = "https://picsum.photos/seed/${Random.nextInt()}/300/200"
+            ),
+            contentDescription = null,
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.large)
+                .fillMaxWidth()
+                .aspectRatio(3f / 2f)
+        )
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = title,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                mainAxisSpacing = 8.dp,
+                mainAxisSize = SizeMode.Wrap,
+
+            ) {
+                AssistChip(
+                    onClick = {  },
+                    colors = AssistChipDefaults.assistChipColors(
+                        leadingIconContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.FavoriteBorder,
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(text = "Mark as Favorite")
+                    }
+                )
+                AssistChip(
+                    onClick = {},
+                    colors = AssistChipDefaults.assistChipColors(
+                        leadingIconContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Share,
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(text = "Share with Others")
+                    }
+                )
+            }
+        }
     }
 }
